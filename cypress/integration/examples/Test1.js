0049fef0c55a7853bdfabc787def8ac58de32606
Aleksandar Jovanovic <aleksandar@Aleksandars-MacBook-Air.local>
2023-06-25 12:03:36 +0200

    Upload all files

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,10 @@
+//cypress - spec
+describe('My First Test Suite', function()
+  {
+    it('My First Test Case', function()
+      {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+      }
+    )
+  }
+)
